b4484b5c61b6f24c548f6b81868738d85664709a
arsan13 <arsan.anbu97@gmail.com>
2022-01-11 11:21:01 +0530

    Added Event Listener for Add button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,11 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text!: string;   // ! denotes required
   @Input() color?: string;  // ? denotes optional
-
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(){
+    this.btnClick.emit()
+  }
 }
